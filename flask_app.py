abe4286a9825f31f6e74465e8f591ff4b5f96aed
amalunnikrishnan <148210703+amalnairr@users.noreply.github.com>
2024-05-19 18:46:23 +0530

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -13,3 +13,6 @@ def predict():
     data = request.get_json()
     prediction = model.predict([data['features']])
     return jsonify({'prediction': int(prediction[0])})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
